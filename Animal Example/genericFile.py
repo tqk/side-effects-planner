7c6a198dcc6fb26c3130cab0bab6b27578575d3c
Jarvis Gexiao Xu <35614031+JarvisGexiaoXu@users.noreply.github.com>
2021-08-17 16:30:28 -0300

    Add files via upload

--- Animal Example/genericFile.py
+++ Animal Example/genericFile.py
@@ -17,46 +17,52 @@ reader.parse_domain(format(a))
 problem = reader.parse_instance(format(b))
 lang = problem.language
 
-
+# add actingAgent sort
+agent = lang.sort('agent')
+achieved_pre = lang.sort('achieved_pre')
 # add done predicate
-donePre = lang.predicate('done')
+donePre = lang.predicate('done',agent)
+#achieved_free = lang.predicate('achieved_free',grid)
 print(type(donePre))
-achieved = lang.predicate('achieved')
+achieved = lang.predicate('achievedA',achieved_pre)
+
+actingAgent = lang.variable('actingAgent',agent)
+
 # add done action
-doneAct = problem.action('done', [], precondition = problem.goal,
+doneAct = problem.action('done', [actingAgent], precondition = problem.goal,
 effects = [
-    iofs.AddEffect(donePre)
+    iofs.AddEffect(donePre(actingAgent))
 ]) 
 
 for x in problem.init.as_atoms():
-    print(type((problem.goal.__dict__)['subformulas'][0]))
     if x not in (problem.goal.__dict__)['subformulas']:
         #   ; If (holding A) is not in the goal, but in the initial state
-        #   (:action achieve_holding_A
+        ##   (:action achieve_holding_A
         #       :precondition (and (done) (holding A))
         #       :effect (and (achieved_holding_A))
         #   )
-        achieved_polluted_gridA = problem.action('achieved_A',[],precondition = donePre & x,
+        const = lang.variable(str(x),achieved_pre) 
+        achieved_A = problem.action('achieved_A_'+str(x),[actingAgent, const],precondition = (donePre(actingAgent)) & x,
             effects = [
-                iofs.AddEffect((achieved))
-        ])
+                iofs.AddEffect(achieved(const))
+            ])
         #   (:action ignore_holding_A
         #       :precondition (and (done))
         #       :effect (and (achieved_holding_A) (increase total-cost 1))
         #   )
         #   ; Must add (achieved_holding_A) to the goal
-        ignore_A = problem.action('ignore_A',[],precondition = donePre,
+        ignore_A = problem.action('ignore_'+str(x),[],precondition = (donePre(actingAgent)),
             effects = [
-                iofs.AddEffect(achieved)
+                iofs.AddEffect(achieved(const))
             ])
         #   ; If (holding A) is not in the goal, and not in the initial state
         #   (:action achieve_holding_A
         #       :precondition (and (done) (not (holding A)))
         #       :effect (and (achieved_holding_A))
         #   )
-        achieved_B = problem.action('achieved_B',[],precondition = donePre &~x,
+        achieved_B = problem.action('achieved_B_'+str(x),[],precondition = (donePre(actingAgent)) & ~x,
             effects = [
-                iofs.AddEffect(achieved)
+                iofs.AddEffect(achieved(const))
             ])
 #export
 #writer = iofs.FstripsWriter(problem)
